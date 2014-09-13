cb2577c443baa55caad31b8d31ff8b1a586109ef
noritan <github@noritan.org>
2014-09-13 18:52:12 +0900

    Initial commit
    Created example project Fifo4Ntan01 as a simple 4-stage FIFO

---
+++ Fifo4Ntan.cydsn/Fifo4Ntan01/Fifo4Ntan01.v
@@ -0,0 +1,117 @@
+
+//`#start header` -- edit after this line, do not edit this line
+// ========================================
+//
+// Copyright noritan.org, 2014
+// All Rights Reserved
+// UNPUBLISHED, LICENSED SOFTWARE.
+//
+// CONFIDENTIAL AND PROPRIETARY INFORMATION
+// WHICH IS THE PROPERTY OF NORITAN.ORG.
+//
+// ========================================
+`include "cypress.v"
+//`#end` -- edit above this line, do not edit this line
+// Generated on 09/13/2014 at 14:22
+// Component: Fifo4Ntan01
+module Fifo4Ntan01 (
+	output  empty,
+	output  full,
+	input   clock,
+	input   reset
+);
+
+//`#start body` -- edit after this line, do not edit this line
+
+// Datapath function declaration
+localparam      CS_IDLE = 3'b000;
+
+// Internal wire
+wire[2:0]   addr = CS_IDLE;             // Datapath function
+wire        not_empty;
+
+cy_psoc3_dp8 #(.cy_dpconfig_a(
+{
+    `CS_ALU_OP_PASS, `CS_SRCA_A0, `CS_SRCB_D0,
+    `CS_SHFT_OP_PASS, `CS_A0_SRC_NONE, `CS_A1_SRC_NONE,
+    `CS_FEEDBACK_DSBL, `CS_CI_SEL_CFGA, `CS_SI_SEL_CFGA,
+    `CS_CMP_SEL_CFGA, /*CFGRAM0:  Idle*/
+    `CS_ALU_OP_PASS, `CS_SRCA_A0, `CS_SRCB_D0,
+    `CS_SHFT_OP_PASS, `CS_A0_SRC_NONE, `CS_A1_SRC_NONE,
+    `CS_FEEDBACK_DSBL, `CS_CI_SEL_CFGA, `CS_SI_SEL_CFGA,
+    `CS_CMP_SEL_CFGA, /*CFGRAM1:  Idle*/
+    `CS_ALU_OP_PASS, `CS_SRCA_A0, `CS_SRCB_D0,
+    `CS_SHFT_OP_PASS, `CS_A0_SRC_NONE, `CS_A1_SRC_NONE,
+    `CS_FEEDBACK_DSBL, `CS_CI_SEL_CFGA, `CS_SI_SEL_CFGA,
+    `CS_CMP_SEL_CFGA, /*CFGRAM2:  Idle*/
+    `CS_ALU_OP_PASS, `CS_SRCA_A0, `CS_SRCB_D0,
+    `CS_SHFT_OP_PASS, `CS_A0_SRC_NONE, `CS_A1_SRC_NONE,
+    `CS_FEEDBACK_DSBL, `CS_CI_SEL_CFGA, `CS_SI_SEL_CFGA,
+    `CS_CMP_SEL_CFGA, /*CFGRAM3:  Idle*/
+    `CS_ALU_OP_PASS, `CS_SRCA_A0, `CS_SRCB_D0,
+    `CS_SHFT_OP_PASS, `CS_A0_SRC_NONE, `CS_A1_SRC_NONE,
+    `CS_FEEDBACK_DSBL, `CS_CI_SEL_CFGA, `CS_SI_SEL_CFGA,
+    `CS_CMP_SEL_CFGA, /*CFGRAM4:  Idle*/
+    `CS_ALU_OP_PASS, `CS_SRCA_A0, `CS_SRCB_D0,
+    `CS_SHFT_OP_PASS, `CS_A0_SRC_NONE, `CS_A1_SRC_NONE,
+    `CS_FEEDBACK_DSBL, `CS_CI_SEL_CFGA, `CS_SI_SEL_CFGA,
+    `CS_CMP_SEL_CFGA, /*CFGRAM5:  Idle*/
+    `CS_ALU_OP_PASS, `CS_SRCA_A0, `CS_SRCB_D0,
+    `CS_SHFT_OP_PASS, `CS_A0_SRC_NONE, `CS_A1_SRC_NONE,
+    `CS_FEEDBACK_DSBL, `CS_CI_SEL_CFGA, `CS_SI_SEL_CFGA,
+    `CS_CMP_SEL_CFGA, /*CFGRAM6:  Idle*/
+    `CS_ALU_OP_PASS, `CS_SRCA_A0, `CS_SRCB_D0,
+    `CS_SHFT_OP_PASS, `CS_A0_SRC_NONE, `CS_A1_SRC_NONE,
+    `CS_FEEDBACK_DSBL, `CS_CI_SEL_CFGA, `CS_SI_SEL_CFGA,
+    `CS_CMP_SEL_CFGA, /*CFGRAM7:  Idle*/
+    8'hFF, 8'hFF,  /*CFG9: */
+    8'hFF, 8'hFF,  /*CFG11-10: */
+    `SC_CMPB_A1_D1, `SC_CMPA_A1_D1, `SC_CI_B_ARITH,
+    `SC_CI_A_ARITH, `SC_C1_MASK_DSBL, `SC_C0_MASK_DSBL,
+    `SC_A_MASK_DSBL, `SC_DEF_SI_0, `SC_SI_B_DEFSI,
+    `SC_SI_A_DEFSI, /*CFG13-12: */
+    `SC_A0_SRC_ACC, `SC_SHIFT_SL, 1'h0,
+    1'h0, `SC_FIFO1_BUS, `SC_FIFO0__A0,
+    `SC_MSB_DSBL, `SC_MSB_BIT0, `SC_MSB_NOCHN,
+    `SC_FB_NOCHN, `SC_CMP1_NOCHN,
+    `SC_CMP0_NOCHN, /*CFG15-14: */
+    7'h00, `SC_FIFO0_DYN_ON,2'h00,
+    `SC_FIFO_CLK__DP,`SC_FIFO_CAP_AX,`SC_FIFO_LEVEL,
+    `SC_FIFO__SYNC /*CFG17-16:   */
+,`SC_EXTCRC_DSBL,`SC_WRK16CAT_DSBL}
+)) dp(
+        /*  input                   */  .reset(reset),
+        /*  input                   */  .clk(clock),
+        /*  input   [02:00]         */  .cs_addr(addr),
+        /*  input                   */  .route_si(1'b0),
+        /*  input                   */  .route_ci(1'b0),
+        /*  input                   */  .f0_load(1'b0),
+        /*  input                   */  .f1_load(1'b0),
+        /*  input                   */  .d0_load(1'b1),
+        /*  input                   */  .d1_load(1'b0),
+        /*  output                  */  .ce0(),
+        /*  output                  */  .cl0(),
+        /*  output                  */  .z0(),
+        /*  output                  */  .ff0(),
+        /*  output                  */  .ce1(),
+        /*  output                  */  .cl1(),
+        /*  output                  */  .z1(),
+        /*  output                  */  .ff1(),
+        /*  output                  */  .ov_msb(),
+        /*  output                  */  .co_msb(),
+        /*  output                  */  .cmsb(),
+        /*  output                  */  .so(),
+        /*  output                  */  .f0_bus_stat(not_empty),
+        /*  output                  */  .f0_blk_stat(full),
+        /*  output                  */  .f1_bus_stat(),
+        /*  output                  */  .f1_blk_stat()
+);
+
+assign      empty = !not_empty;
+
+//`#end` -- edit above this line, do not edit this line
+endmodule
+//`#start footer` -- edit after this line, do not edit this line
+//`#end` -- edit above this line, do not edit this line
+
+
---
+++ Fifo4Ntan.cydsn/main.c
@@ -0,0 +1,70 @@
+/* ========================================
+ *
+ * Copyright noritan.org, 2014
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF NORITAN.ORG.
+ *
+ * ========================================
+*/
+#include <project.h>
+
+uint8       v[8] = {0};
+
+void show(void) {
+    uint8       i;
+    
+    LCD_Position(0, 0);
+    for (i = 0; i < 8; i++) {
+        LCD_PrintInt8(v[i]);
+    }
+    LCD_Position(1, 0);
+    if (SR1_Read() & 1) {
+        LCD_PrintString("FULL ");
+    } else {
+        LCD_PrintString("     ");
+    }
+    if (SR1_Read() & 2) {
+        LCD_PrintString("EMPTY ");
+    } else {
+        LCD_PrintString("      ");
+    }
+}
+
+int main()
+{
+    uint8       x = 1;
+    uint8       i;
+    
+    CR1_Write(0);
+    
+    LCD_Start();
+    
+    /* CyGlobalIntEnable; */ /* Uncomment this line to enable global interrupts. */
+    
+    show();
+    
+    for(;;) {
+        if (!SW2_Read()) {
+            CY_SET_REG8(Fifo_dp_u0__F0_REG, x++);
+            show();
+            while (!SW2_Read()) {
+                CyDelay(10);
+            }
+        }
+        if (!SW3_Read()) {
+            for (i = 0; i < 7; i++) {
+                v[i] = v[i+1];
+            }
+            v[7] = CY_GET_REG8(Fifo_dp_u0__F0_REG);
+            show();
+            while (!SW3_Read()) {
+                CyDelay(10);
+            }
+        }
+    }
+}
+
+/* [] END OF FILE */
